dfd05ac185264dd073846b167f62279ccefa29a3
Maico Olguin <168041229+cuentahitgub1@users.noreply.github.com>
2024-09-18 20:43:03 -0300

    Add files via upload

---
+++ maicoScript.js
@@ -0,0 +1,5 @@
+function toggleBoxes() {
+    var box1 = document.getElementById('box1').parentElement;
+    var box2 = document.getElementById('box2');
+    box1.classList.toggle('hidden');
+    box2.classList.toggle('hidden');}
